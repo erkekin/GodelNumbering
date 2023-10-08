b0ea3755e851413d334ad622f28c47c3c3d2ca32
Erk Ekin <erkekin@gmail.com>
2023-10-08 11:42:43 +0100

    subst

--- Sources/GodelNumbering/GodelNumbering.swift
+++ Sources/GodelNumbering/GodelNumbering.swift
@@ -2,6 +2,36 @@ import ExpressionTree
 import BinaryTree
 
 extension ExpressionTree where Descendent == BinaryChildren<Token>  {
+  private static let primesBig: [Int] = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+  
+  fileprivate static func subst(_ expresions: [ExpressionTree]) -> ExpressionTree {
+    var treeFactors = Array(expresions.enumerated())
+    let firstFactor = treeFactors.removeFirst()
+    let firstFactorTree = ExpressionTree.node(value: .funct(.exponential), .init(.leaf(.num(Self.primesBig[firstFactor.offset])), firstFactor.element))
+
+    return treeFactors
+      .map{ index, exponential in
+          .node(value: .funct(.exponential), .init(.leaf(.num(Self.primesBig[index])), exponential))
+      }
+      .reduce(firstFactorTree) { partialResult, tree in
+          .node(value: .funct(.multiply), .init(partialResult, tree))
+      }
+  }
+
+  public static func build(exponentials: [Int]) -> ExpressionTree {
+    var factors = Array(exponentials.enumerated())
+    let firstFactor = factors.removeFirst()
+    let firstFactorTree = ExpressionTree.node(value: .funct(.exponential), .init(.leaf(.num(Self.primesBig[firstFactor.offset])), .leaf(.num(firstFactor.element))))
+    
+    return factors
+      .map{ index, exponential in
+          .node(value: .funct(.exponential), .init(.leaf(.num(Self.primesBig[index])), .leaf(.num(exponential))))
+      }
+      .reduce(firstFactorTree) { partialResult, tree in
+          .node(value: .funct(.multiply), .init(partialResult, tree))
+      }
+  }
+
   func hasPrefix(factor branch: ExpressionTree) -> Bool {
     guard self != branch else { return true }
     guard case .funct(.multiply) = value, let descendent = descendent else { return false }
@@ -14,6 +44,22 @@ extension ExpressionTree where Descendent == BinaryChildren<Token>  {
       if descendent.right.hasPrefix(factor: branch) { return true }
       return false
     }
+  }
+  
+  public static func sub(self: [Int], int: Int, proof: [Int]) -> ExpressionTree {
+    let expressions: [ExpressionTree] = self.map { exponential in
+      int == exponential ? .build(exponentials: proof) : .leaf(.num(exponential))
+    }
+    
+    return ExpressionTree.subst(expressions)
+  }
+  
+  public static func selfSubstitute(self: [Int], int: Int) -> ExpressionTree {
+    let expressions: [ExpressionTree] = self.map { exponential in
+      int == exponential ? .build(exponentials: self) : .leaf(.num(exponential))
+    }
+    
+    return ExpressionTree.subst(expressions)
   }
 }
 
--- Tests/GodelNumberingTests/GodelNumberingTests.swift
+++ Tests/GodelNumberingTests/GodelNumberingTests.swift
@@ -3,13 +3,13 @@ import ExpressionTree
 @testable import GodelNumbering
 
 final class GodelNumberingTests: XCTestCase {
-
+  
   func test_hasFactorSimple() {
     let tree = ExpressionTree(prefixExpression: "^ 2 3")
     
     XCTAssertTrue(tree.hasPrefix(factor: tree))
   }
-
+  
   func test_hasFactor() {
     let tree = ExpressionTree(prefixExpression: "* ^ 2 3 ^ 3 2")
     let left = ExpressionTree(prefixExpression: "^ 2 3")
@@ -18,7 +18,7 @@ final class GodelNumberingTests: XCTestCase {
     XCTAssertTrue(tree.hasPrefix(factor: left))
     XCTAssertTrue(tree.hasPrefix(factor: right))
   }
-
+  
   func test_hasFactorComplex() {
     let tree = ExpressionTree(prefixExpression: "* * * ^ 2 3 ^ 3 2 ^ 5 4 ^ 7 1")
     let _7 = ExpressionTree(prefixExpression: "^ 7 1")
@@ -34,4 +34,59 @@ final class GodelNumberingTests: XCTestCase {
     XCTAssertTrue(tree.hasPrefix(factor: _5))
     XCTAssertTrue(tree.hasPrefix(factor: _2and3))
   }
+  
+  func test_hasFactorComplex2() {
+    let tree = ExpressionTree(prefixExpression:
+                                "* * * * * * * * * ^ 2 8 ^ 3 4 ^ 5 13 ^ 7 9 ^ 11 8 ^ 13 13 ^ 17 5 ^ 19 7 ^ 23 71 ^ 29 9")
+    XCTAssertTrue(tree.hasPrefix(factor: ExpressionTree(prefixExpression: "^ 2 8")))
+    XCTAssertTrue(tree.hasPrefix(factor: ExpressionTree(prefixExpression: "^ 3 4")))
+    XCTAssertTrue(tree.hasPrefix(factor: ExpressionTree(prefixExpression: "^ 5 13")))
+    XCTAssertTrue(tree.hasPrefix(factor: ExpressionTree(prefixExpression: "^ 7 9")))
+    XCTAssertTrue(tree.hasPrefix(factor: ExpressionTree(prefixExpression: "^ 11 8")))
+    XCTAssertTrue(tree.hasPrefix(factor: ExpressionTree(prefixExpression: "^ 13 13")))
+    XCTAssertTrue(tree.hasPrefix(factor: ExpressionTree(prefixExpression: "^ 17 5")))
+    XCTAssertTrue(tree.hasPrefix(factor: ExpressionTree(prefixExpression: "^ 19 7")))
+    XCTAssertTrue(tree.hasPrefix(factor: ExpressionTree(prefixExpression: "^ 23 71")))
+    XCTAssertTrue(tree.hasPrefix(factor: ExpressionTree(prefixExpression: "^ 29 9")))
+    
+    Swift.print(tree.description)
+  }
+  
+  func test_init() {
+    let tree = ExpressionTree(prefixExpression:
+                                "* * * * * * * * * ^ 2 8 ^ 3 4 ^ 5 13 ^ 7 9 ^ 11 8 ^ 13 13 ^ 17 5 ^ 19 7 ^ 23 17 ^ 29 9")
+    XCTAssertTrue(tree.hasPrefix(factor: ExpressionTree(prefixExpression: "^ 2 8")))
+    XCTAssertTrue(tree.hasPrefix(factor: ExpressionTree(prefixExpression: "^ 3 4")))
+    XCTAssertTrue(tree.hasPrefix(factor: ExpressionTree(prefixExpression: "^ 5 13")))
+    XCTAssertTrue(tree.hasPrefix(factor: ExpressionTree(prefixExpression: "^ 7 9")))
+    XCTAssertTrue(tree.hasPrefix(factor: ExpressionTree(prefixExpression: "^ 11 8")))
+    XCTAssertTrue(tree.hasPrefix(factor: ExpressionTree(prefixExpression: "^ 13 13")))
+    XCTAssertTrue(tree.hasPrefix(factor: ExpressionTree(prefixExpression: "^ 17 5")))
+    XCTAssertTrue(tree.hasPrefix(factor: ExpressionTree(prefixExpression: "^ 19 7")))
+    XCTAssertTrue(tree.hasPrefix(factor: ExpressionTree(prefixExpression: "^ 23 17")))
+    XCTAssertTrue(tree.hasPrefix(factor: ExpressionTree(prefixExpression: "^ 29 9")))
+    
+    XCTAssertEqual(tree, ExpressionTree.build(exponentials: [8, 4, 13, 9, 8, 13, 5, 7, 17, 9]))
+  }
+  
+  func test_sub() {
+    let expected_substituted3_to_self = ExpressionTree(prefixExpression:"* ^ 2 * ^ 2 3 ^ 3 2 ^ 3 2")
+    let actual = ExpressionTree.sub(self: [3, 2], int: 3, proof: [3, 2])
+    
+    XCTAssertEqual(actual, expected_substituted3_to_self)
+  }
+  
+  func test_complex_selfSubstitute() {
+    let expected = ExpressionTree(prefixExpression: """
+* * * * * * * * * ^ 2 8 ^ 3 4 ^ 5 13 ^ 7 9 ^ 11 8 ^ 13 13 ^ 17 5 ^ 19 7 ^ 23 \
+* * * * * * * * * ^ 2 8 ^ 3 4 ^ 5 13 ^ 7 9 ^ 11 8 ^ 13 13 ^ 17 5 ^ 19 7 ^ 23 17 ^ 29 9 \
+^ 29 9
+"""
+    )
+    
+    let actual = ExpressionTree.selfSubstitute(self: [8, 4, 13, 9, 8, 13, 5, 7, 17, 9], int: 17)
+    
+    XCTAssertEqual(actual, expected)
+  }
 }
+
